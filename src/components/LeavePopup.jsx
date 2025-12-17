86ca04ca7d90a517f23e70bb8184c1363020a2e4
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2025-12-17 17:31:26 +0530

    backend url updated

--- src/components/LeavePopup.jsx
+++ src/components/LeavePopup.jsx
@@ -24,7 +24,7 @@ const LeavePopup = ({ onClose }) => {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:3001/send-leave', {
+            const response = await fetch('https://mnet-3c33.vercel.app/send-leave', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
